2367dc9234d1c8cb39a3a42789ede644d03388b2
hmtey <huimingtey@u.nus.edu>
2020-04-05 10:31:09 +0800

    Added routes

---
+++ server/routes/deliveryRiderAPI/riderRoutes.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var riRouter = express.Router();
+
+//work schedule
+const getWorkSchedule = require();
+const updateWorkSchedule = require();
+
+//delivery
+const getOrders = require();
+const updateDelivery = require();
+const get
+
+
+riRouter.route(`/schedule`)
+
+riRouter.route(`/orders`)
