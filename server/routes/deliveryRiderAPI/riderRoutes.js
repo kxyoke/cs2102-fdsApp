69bb0ae062170b965e06f10f6e40453670fafd54
hmtey <huimingtey@u.nus.edu>
2020-04-05 12:09:18 +0800

    Added methods

--- server/routes/deliveryRiderAPI/riderRoutes.js
+++ server/routes/deliveryRiderAPI/riderRoutes.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var riRouter = express.Router();
 
+const getProfile = require(`./methods/getProfile`);
+const updateProfile = require(`./methods/updateProfile`);
+
 //work schedule
-const getWorkSchedule = require();
-const updateWorkSchedule = require();
+const getWorkSchedule = require(`./methods/getWorkSchedule`);
+const updateWorkSchedule = require(`./methods/updateWorkSchedule`);
 
 //delivery
-const getOrders = require();
-const updateDelivery = require();
-const get
+const getDeliveries = require(`./methods/getDeliveries`);
+const getOrder = require(`./methods/getOrder`);
+const updateDeliveryTime = require(`./methods/updateDeliveryTime`);
 
+riRouter.route(`/:riid`)
+   .get(getProfile)
+   .put(updateProfile);
 
-riRouter.route(`/schedule`)
+riRouter.route(`/schedule/:riid`)
+   .get(getWorkSchedule)
+   .put(updateWorkSchedule);
 
-riRouter.route(`/orders`)
+riRouter.route(`/deliveries/:riid`)
+   .get(getDeliveries);
+
+riRouter.route(`/deliveries/:riid/:oid`)
+    .get(getOrder)
+    .put(updateDeliveryTime);
