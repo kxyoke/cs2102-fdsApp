3dfe0b51256f8ca60037f964558237ce24793f36
hmtey <huimingtey@u.nus.edu>
2020-04-05 10:52:53 +0800

    Updated API

--- server/routes/fdsManagerAPI/managerRoutes.js
+++ server/routes/fdsManagerAPI/managerRoutes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var mRouter = express.Router();
 
+const getProfile = require(`./methods/getProfile`);
+const updateProfile = require(`./methods/updateProfile`);
 const addCoupon = require(`./methods/addCoupon`);
 const getCoupon = require(`./methods/getCoupon`);
 const updateCoupon = require(`./methods/updateCoupon`);
@@ -8,6 +10,10 @@ const addFdsManagerPromo = require(`./methods/addPromo`);
 const getAllFdsManagerPromos = require(`./methods/getAllPromos`);
 const getFdsManagerPromo = require(`./methods/getPromo`);
 const updateFdsManagerPromo = require(`./methods/updatePromo`);
+
+mRouter.route(`/:mid`)
+    .get(getProfile)
+    .put(updateProfile)
 
 mRouter.route(`/coupon/`)
     .post(addCoupon)
