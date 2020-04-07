977311a085bd8b2191ea54bea8183cf832310969
Yoke Kai Xin <kxyoke@gmail.com>
2020-04-07 17:44:23 +0800

    Add partial skeleton for restaurant sql queries.

--- server/routes/restaurantAPI/methods/addMenuFoodItem.js
+++ server/routes/restaurantAPI/methods/addMenuFoodItem.js
@@ -1,14 +1,35 @@
 const pool = require('../../../db'); // psql db
 const log = require('../../../logger')
+const rsql = require('../../../sql/restaurantQueries');
 
+const shortid = require('shortid');
+
+/* Req params should have:
+ *  - rid
+ * Req body should have:
+ *  - name
+ *  - description
+ *  - price
+ *
+ * Res should contain:
+ *  - fid != NULL, if successful.
+ */
 module.exports = (req, res) => {
-/*
-    pool.query('SELECT * FROM Restaurants',
+    log.info('Querying add rMenuItem.');
+    const rid = req.params.rid;
+    const fname = req.body.name;
+    const fdesc = req.body.description;
+    const price = req.body.price;
+
+    const fid = shortid.generate(); // assume no collisions given low rate
+
+    pool.query(rsql.queries.add.menuItem, [TODO],
         (q_err, q_res) => {
             res.json(q_res.rows)
-        });
+        })
+};
+
+/*
 //https://www.freecodecamp.org/news/fullstack-react-blog-app-with-express-and-psql/
 */
-    res.send('Queried add rMenuItem.');
-};
 
--- server/routes/restaurantAPI/methods/getFoodItem.js
+++ server/routes/restaurantAPI/methods/getFoodItem.js
@@ -1,7 +1,8 @@
 const pool = require('../../../db'); // psql db
 const log = require('../../../logger');
+const rsql = require('../../../sql/restaurantQueries');
 
-/* Req body should have:
+/* Req params should have:
  *  - rid
  *  - fid
  */
@@ -9,12 +10,11 @@ module.exports = (req, res) => {
     log.info('Querying rMenuItem')
     const rid = req.params.rid;
     const fid = req.params.fid;
-    const my_query = `SELECT * FROM MenuItems NATURAL JOIN FoodItems WHERE food_id = ${fid} AND res_id = ${rid}`;
-    pool.query(my_query,
+
+    pool.query(rsql.queries.get.foodItem, [fid, rid],
         (q_err, q_res) => {
             res.json(q_res.rows)
         })
-    //res.send('Queried get rMenuItem.');
 };
 
 /*
