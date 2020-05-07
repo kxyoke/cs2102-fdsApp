8ae401f563639ead08958d68f46182cb696a3c6f
ShuTingY <shuting_y@u.nus.edu>
2020-05-07 08:37:16 +0800

    edit the average to round to 2 decimal place and replace null with 0

--- client/src/components/fds/components/riderSummary.js
+++ client/src/components/fds/components/riderSummary.js
@@ -3,15 +3,22 @@ import { Table } from 'semantic-ui-react'
 
 export default function RiderSummary(props) {
     const { rider_id, salary, total_deliveries, avg_delivery_time, total_ratings, averge_rating } = props.riderSummary;
-    
+    function roundToTwo(num) {
+        console.log(num);
+        if(num === undefined || num === null) {
+            return 0;
+        }
+        return +(Math.round(num + "e+2")  + "e-2");
+        
+    }
     return (
             <Table.Row>
                 <Table.Cell>{rider_id}</Table.Cell>
                 <Table.Cell>{salary}</Table.Cell>
                 <Table.Cell>{total_deliveries}</Table.Cell>
-                <Table.Cell>{avg_delivery_time}</Table.Cell>
-                <Table.Cell>{total_ratings}</Table.Cell>
-                <Table.Cell>{averge_rating}</Table.Cell>
+                <Table.Cell>{roundToTwo(avg_delivery_time)}</Table.Cell>
+                <Table.Cell>{total_ratings === null? 0 : total_ratings}</Table.Cell>
+                <Table.Cell>{roundToTwo(averge_rating)}</Table.Cell>
             </Table.Row>
     )
 }
