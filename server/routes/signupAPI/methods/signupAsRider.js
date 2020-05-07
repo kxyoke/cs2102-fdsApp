c2c697b661e14a56ca9bd07da26303f46455cc68
MC <dandford@gmail.com>
2020-05-07 19:18:10 +0800

    Amend merge conflicts

--- server/routes/signupAPI/methods/signupAsRider.js
+++ server/routes/signupAPI/methods/signupAsRider.js
@@ -1,52 +1,52 @@
 const pool = require('../../../db'); // psql db
 const sql = require('../../../sql');
-const log = require('../../../logger');
 const bcrypt = require('bcrypt');
 const shortid = require('shortid');
 
 module.exports =async (req, res) => {
+    console.log(req.body);
     var hash = await bcrypt.hash(req.body.password, 10);
     const id = shortid.generate();
-
-    switch (req.body.riderType) {
-        case 'full':
-            pool.query(sql.users.function.addFullTimeRider, 
-                    [id, req.body.username, hash],
-                     (err) => {
-                    if(err) {
-                        log.error(err.message);
-                        if(err.message === 'username in used') {
-                            return res.status(422).send(err.message);
-                        }  else if (err.message === 'usr_id in used') {
-                            //do something
-                        } else {
-                            log.error('database err found')
-                            return res.sendStatus(500);
-                        }
-
-                    })
-                    break;
-        case 'part':
-
-               
-            pool.query(sql.users.function.addPartTimeRider, 
-                [id, req.body.username, hash],
-                 (err) => {
-                if(err) {
-                    log.error(err.message);
-                    if(err.message === 'username in used') {
+    if (req.body.riderType === 'full') {
+        console.log(id, hash, req.body.username);
+        console.log('fulltime')
+        pool.query(sql.users.function.addFullTimeRider,
+            [id, req.body.username, hash],
+            (err) => {
+                if (err) {
+                    console.log(err.message);
+                    if (err.message === 'username in used') {
                         return res.status(422).send(err.message);
-                    }  else if (err.message === 'usr_id in used') {
+                    } else if (err.message === 'usr_id in used') {
                         //do something
                     } else {
-                        log.error('database err found')
+                        console.log('database err found')
                         return res.sendStatus(500);
                     }
                 } else {
                     return res.sendStatus(200);
                 }
             })
-            break;
+    } else {
+        console.log(id, hash, req.body.username);
+        console.log('parttime')
+        pool.query(sql.users.function.addPartTimeRider,
+            [id, req.body.username, hash],
+            (err) => {
+                if(err) {
+                    console.log(err.message);
+                    if(err.message === 'username in used') {
+                        return res.status(422).send(err.message);
+                    }  else if (err.message === 'usr_id in used') {
+                        //do something
+                    } else {
+                        console.log('database err found')
+                        return res.sendStatus(500);
+                    }
+                } else {
+                    return res.sendStatus(200);
+                }
+            })
+
     }
 };
-
