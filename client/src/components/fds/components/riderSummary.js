d5619259d4b11d0c45d807c953c44abc67ba2029
ShuTingY <shuting_y@u.nus.edu>
2020-05-07 09:31:57 +0800

    change the rider summary, correct typo

--- client/src/components/fds/components/riderSummary.js
+++ client/src/components/fds/components/riderSummary.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Table } from 'semantic-ui-react'
 
 export default function RiderSummary(props) {
-    const { rider_id, salary, total_deliveries, avg_delivery_time, total_ratings, averge_rating } = props.riderSummary;
+    const { rider_id, salary, total_deliveries, avg_delivery_time, total_ratings, average_rating } = props.riderSummary;
     function roundToTwo(num) {
-        if(num === undefined || num === null) {
-            return 0;
-        }
+        console.log(num);
         return +(Math.round(num + "e+2")  + "e-2");
         
     }
@@ -14,10 +12,10 @@ export default function RiderSummary(props) {
             <Table.Row>
                 <Table.Cell>{rider_id}</Table.Cell>
                 <Table.Cell>{salary}</Table.Cell>
-                <Table.Cell>{total_deliveries===null? 0: total_deliveries}</Table.Cell>
+                <Table.Cell>{total_deliveries}</Table.Cell>
                 <Table.Cell>{roundToTwo(avg_delivery_time)}</Table.Cell>
-                <Table.Cell>{total_ratings === null? 0 : total_ratings}</Table.Cell>
-                <Table.Cell>{roundToTwo(averge_rating)}</Table.Cell>
+                <Table.Cell>{total_ratings}</Table.Cell>
+                <Table.Cell>{roundToTwo(average_rating)}</Table.Cell>
             </Table.Row>
     )
 }
