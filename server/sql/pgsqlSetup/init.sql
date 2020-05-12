4ac18f4cfd4929de6d98fc2ef3fd2f3ce35177fd
Yoke Kai Xin <kxyoke@gmail.com>
2020-05-12 20:01:47 +0800

    Update init.sql to use Taylor for res demo.

--- server/sql/pgsqlSetup/init.sql
+++ server/sql/pgsqlSetup/init.sql
@@ -1156,11 +1156,11 @@ INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,dest
 INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (53,'qnXAuDyjP9',2,'cash',ARRAY [['21','2']],'complete',17.81,'20 Jalan Kerluke Place', '310524');
 INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (54,'qnXAuDyjP9',29,'card',ARRAY [['4','4']],'complete', 102.76,'20 Jalan Kerluke Place', '310524');
 
-INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (55,'XoBAl4RsbO',40,'card',ARRAY [['10','1'],['5','3'],['1','1']],'complete', 28.40, '9 Howard Road 04-00', '787820');
-INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (56,'XoBAl4RsbO',40,'card',ARRAY [['5','3'],['3','6']],'complete', 49.40, '9 Howard Road 04-00', '787820');
+INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (55,'XoBAl4RsbO',5,'card',ARRAY [['10','1'],['5','3'],['1','1']],'complete', 28.40, '9 Howard Road 04-00', '787820');
+INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (56,'XoBAl4RsbO',5,'card',ARRAY [['5','3'],['3','6']],'complete', 49.40, '9 Howard Road 04-00', '787820');
 INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (57,'XoBAl4RsbO',40,'card',ARRAY [['2','1'],['6','1']],'complete', 23.20, '9 Howard Road 04-00', '787820');
 INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (58,'XoBAl4RsbO',40,'card',ARRAY [['7','2']],'complete', 15.35, '9 Howard Road 04-00', '787820');
-INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (59,'XoBAl4RsbO',40,'card',ARRAY [['4','3']],'in progress', 15.35, '9 Howard Road 04-00', '787820');
+INSERT INTO Orders (order_id,usr_id,res_id,payment,listOfItems,status,total,destination_address, postal_code ) VALUES (59,'XoBAl4RsbO',5,'card',ARRAY [['4','3']],'in progress', 15.35, '9 Howard Road 04-00', '787820');
 
 
 INSERT INTO reviews VALUES(2, 'The food is good', 5);
@@ -1310,8 +1310,8 @@ INSERT INTO Promotions (pid,promotype, res_id, description,start_day,end_day) VA
 INSERT INTO Promotions (pid,promotype,description,start_day,end_day) VALUES (2,'FDS','Delivery:percent;100','2020-01-31 11:45:19','2020-02-07 11:45:19');
 INSERT INTO Promotions (pid,promotype,description,start_day,end_day) VALUES (3,'FDS','Discount:dollars;8','2020-01-21 11:45:19','2020-01-30 11:45:19');
 INSERT INTO Promotions (pid,promotype,description,start_day,end_day) VALUES (4,'FDS','Discount:percent;20','2020-03-31 12:00:19','2020-04-22 12:00:19');
-INSERT INTO Promotions (pid,promotype,res_id,description,start_day,end_day) VALUES (6,'RES', 21, 'DEFAULT:percent;3;10','2019-05-31 22:00:19','2019-08-31 22:00:19');
-INSERT INTO Promotions (pid,promotype,res_id,description,start_day,end_day) VALUES (7,'RES', 32, 'DEFAULT:absolute;3;2','2020-01-01 00:00:00','2020-12-31 00:00:00');
+INSERT INTO Promotions (pid,promotype,res_id,description,start_day,end_day) VALUES (6,'RES', 5, 'DEFAULT:percent;3;10','2019-05-31 22:00:19','2019-08-31 22:00:19');
+INSERT INTO Promotions (pid,promotype,res_id,description,start_day,end_day) VALUES (7,'RES', 5, 'DEFAULT:absolute;3;2','2020-01-01 00:00:00','2020-05-31 00:00:00');
 INSERT INTO Promotions (pid,promotype,description,start_day,end_day) VALUES (8,'FDS','Delivery:percent;100','2020-03-01 00:00:00','2020-03-25 00:00:00');
 INSERT INTO Promotions (pid,promotype,description,start_day,end_day) VALUES (9,'FDS','Delivery:percent;100','2020-04-23 00:00:00','2020-12-31 00:00:00');
 INSERT INTO Promotions (pid,promotype,description,start_day,end_day) VALUES (10,'FDS','Discount:dollars;10','2019-11-03 00:00:00','2019-11-29 00:00:00');
